c02f325f1e07a67d2156f22dd357db7d9efb1980
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2023-01-07 14:25:00 +0000

    Alpha work on managing App.config and establishing solution to run as a service.

--- run-as-admin-to-start-as-a-win7-service.cmd
+++ run-as-admin-to-start-as-a-win7-service.cmd
@@ -1,3 +1,5 @@
+sc delete OscalertSvc
 sc create OscalertSvc binpath= "C:\Program Files\Kalips'o Infogistics LLC\OscalertSvc\bin\OscalertSvc.exe" start= auto displayname= "OscalertSvc"
 sc description OscalertSvc "OscalertSvc - automatic near-realtime cellphone notifications about certain VBRescue field situations"
 sc start OscalertSvc
+pause
