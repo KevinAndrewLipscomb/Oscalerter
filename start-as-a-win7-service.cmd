d1f58368f54e429e703214f9e26a615f9587cd78
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2023-01-07 11:41:59 +0000

    Alpha work on establishing as a service.

---
+++ start-as-a-win7-service.cmd
@@ -0,0 +1,3 @@
+sc create OscalertSvc binpath= "C:\Program Files\Kalips'o Infogistics LLC\OscalertSvc\bin\OscalertSvc.exe" start= auto displayname= "OscalertSvc"
+sc description OscalertSvc "OscalertSvc - automatic near-realtime cellphone notifications about certain VBRescue field situations"
+sc start OscalertSvc
