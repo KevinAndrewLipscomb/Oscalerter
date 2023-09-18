c0891d678b87966b3ba89ea7578abcb27e4950f6
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2023-09-18 19:07:04 -0400

    Removed unnecessary runtime assemblyBinding redirects.  Removed direct package references that could be inherited instead.  Revised work.bat to work with git instead of SVN.

--- work.bat
+++ work.bat
@@ -2,6 +2,6 @@ REM
 REM $Id$
 REM
 cd "C:\Program Files\Kalips'o Infogistics LLC\OscalertSvc"
-start /max explorer /e,/select,"C:\Program Files\Kalips'o Infogistics LLC\OscalertSvc\.svn"
+start /max explorer /e,/select,"C:\Program Files\Kalips'o Infogistics LLC\OscalertSvc\.git"
 start /max OscalertSvc.sln
 IF EXIST "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe" (start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe") ELSE start "" /max "C:\Program Files (x86)\MySQL\MySQL Workbench\MySQLWorkbench.exe"
