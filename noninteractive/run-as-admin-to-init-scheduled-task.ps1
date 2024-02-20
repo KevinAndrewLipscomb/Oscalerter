c0eddaf035de72e5f37dddd12f0a76444c009179
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2024-02-20 14:13:57 -0500

    Reordered some statements.

--- noninteractive/run-as-admin-to-init-scheduled-task.ps1
+++ noninteractive/run-as-admin-to-init-scheduled-task.ps1
@@ -1,11 +1,13 @@
 $taskName = "Oscalerter"
+Stop-ScheduledTask -TaskName $taskName |
+  Unregister-ScheduledTask
+#
 $action = New-ScheduledTaskAction -Execute "${PsScriptRoot}\..\bin\Oscalerter.exe"
 $description = "Near real-time notifications for certain field situations"
 $trigger = New-ScheduledTaskTrigger -AtStart
-Stop-ScheduledTask -TaskName $taskName |
-  Unregister-ScheduledTask
 $principal = New-ScheduledTaskPrincipal -LogonType S4U -UserId $env:UserName
 New-ScheduledTask -Action $action -Description $description -Principal $principal -Trigger $trigger |
   Register-ScheduledTask -Force -TaskName $taskName |
   Start-ScheduledTask
+#
 Invoke-Item ${env:windir}\system32\taskschd.msc
