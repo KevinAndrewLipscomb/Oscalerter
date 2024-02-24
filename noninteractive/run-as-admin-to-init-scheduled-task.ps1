964b1947e09c33425e0c27018d200ff86363c56a
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2024-02-24 16:30:26 -0500

    Revised to make ExecutionTimeLimit infinite, to maximize RestartCount, and to bind argument names to values with colon characters.

--- noninteractive/run-as-admin-to-init-scheduled-task.ps1
+++ noninteractive/run-as-admin-to-init-scheduled-task.ps1
@@ -1,13 +1,14 @@
 $taskName = "Oscalerter"
-Stop-ScheduledTask -TaskName $taskName |
+Stop-ScheduledTask -TaskName:$taskName |
   Unregister-ScheduledTask
 #
-$action = New-ScheduledTaskAction -Execute "${PsScriptRoot}\..\bin\Oscalerter.exe"
+$action = New-ScheduledTaskAction -Execute:"${PsScriptRoot}\..\bin\Oscalerter.exe"
 $description = "Near real-time notifications for certain field situations"
+$principal = New-ScheduledTaskPrincipal -LogonType:S4U -UserId:$env:UserName
+$settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit:0 -RestartCount:([uint16]::MaxValue) -RestartInterval:00:05:00
 $trigger = New-ScheduledTaskTrigger -AtStart
-$principal = New-ScheduledTaskPrincipal -LogonType S4U -UserId $env:UserName
-New-ScheduledTask -Action $action -Description $description -Principal $principal -Trigger $trigger |
-  Register-ScheduledTask -Force -TaskName $taskName |
+New-ScheduledTask -Action:$action -Description:$description -Principal:$principal -Settings:$settings -Trigger:$trigger |
+  Register-ScheduledTask -Force -TaskName:$taskName |
   Start-ScheduledTask
 #
 Invoke-Item ${env:windir}\system32\taskschd.msc
