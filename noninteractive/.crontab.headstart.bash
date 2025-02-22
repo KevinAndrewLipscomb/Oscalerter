7a0fec510238865eb6038bc498ff10f6ada22dc7
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2025-02-22 13:46:08 -0500

    Revised to account for actual deployment practices.

--- noninteractive/.crontab.headstart.bash
+++ noninteractive/.crontab.headstart.bash
@@ -1,6 +1,6 @@
 echo ""
 echo "${0}: Launching Oscalerter as a daemon..."
-nohup /cygdrive/c/program-files/Ki/Oscalerter/bin/Oscalerter.exe &
+nohup /cygdrive/c/PROJ/Oscalerter/bin/Oscalerter.exe &
 echo "${0}: Done."
 echo "${0}: Make sure its entry is in your crontab so that it will run as a cron job upon @reboot."
 echo ""
